9adbe668bf360ce570692d8be409ca95164c3970
vinodh nartu <vinodhn@smbxl.com>
2023-01-02 22:58:03 +0530

    profile component

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
 import { TimesheetComponent } from './components/timesheet/timesheet.component';
 
 const routes: Routes = [
   {
     path:"timesheet",component:TimesheetComponent
+  }
+  ,{
+    path:"profile",component:ProfileComponent
   }
 ];
 
